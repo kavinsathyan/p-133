4a711d4df8a7dcb0cdcde6fb7b6da7a752a0be61
kavinsathyan <79074982+kavinsathyan@users.noreply.github.com>
2021-10-02 11:19:52 +0530

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,53 @@
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('clock.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(540, 200);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting Objects";
+}
+
+
+function modelLoaded()
+{
+    console.log("model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(img,0,0,640,250)
+    if(status != "")
+    {
+      for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "status : Objects Detected";
+
+          fill("#800080");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15 );
+          noFill();
+          stroke("#800080");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }  
+    }
+
+
+}
---
+++ bag.js
@@ -0,0 +1,53 @@
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('bag.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 250);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+
+
+function modelLoaded()
+{
+    console.log("model Loaded!");
+    status= true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(img,0,0,640,250)
+    if(status != "")
+    {
+      for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "status : Objects Detected";
+
+          fill("#800080");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15 );
+          noFill();
+          stroke("#800080");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }  
+    }
+
+
+}
